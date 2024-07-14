d643d4e6a0cda4542f2313d8926f2f13ef9a0f53
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2024-07-14 12:23:32 +0200

    rename arxive to correct 'arxiv'

--- src/info_srcs/aggregate.py
+++ src/info_srcs/aggregate.py
@@ -1,17 +1,17 @@
-from .arxive import search_arxive
+from .arxiv import search_arxiv
 
 
 def aggregate(topics: list[str], days_back: int) -> list[str]:
     """
     A function that aggregates information on provided topics from the sources in `info_srcs` to be passed to the distillment pipeline.
     Information is gathered from the sources via webscrapping or API calls.
-    topics: a list of the topics that you want to search. The list from the arxive can be found here: https://arxiv.org/category_taxonomy
+    topics: a list of the topics that you want to search. The list from the arxiv can be found here: https://arxiv.org/category_taxonomy
     days_back: the number of days back to search for papers
     """
     print("Aggregating information on the following topics:", topics)
 
     info = []
     for topic in topics:
-        info.append(search_arxive(topic, days_back))
+        info.append(search_arxiv(topic, days_back))
 
     return info
