e0f7c2992c9a2085391e5ee6cefe3d0d8ac27a97
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2024-07-14 12:22:47 +0200

    Implement summarization

--- src/distill.py
+++ src/distill.py
@@ -1,15 +1,31 @@
 import google.generativeai as genai
 from credentials import get_gemini_key
+import textwrap
 
 
 def distill(
-    user_bg: str, text_body: list[str], model: str = "gemini-1.5-pro-latest"
-) -> str:
+    user_bg: str, text_body: list[str], model_name: str = "models/gemini-1.5-pro-latest"
+) -> None:
     """
     A function that distills the heap of information gathered for the user into a single digestible and useful piece of information.
     Accepts the user's background (str) and the list of text bodies(list[str]) and returns a single piece of markdown.
     Markdown is produced by a gemini model (specified by the model parameter)
     """
-    gemini_key: str = get_gemini_key()
 
-    return ""
+    gemini_key: str = get_gemini_key()
+    genai.configure(api_key=gemini_key)
+    model = genai.GenerativeModel(model_name)
+
+    general_instruction = "General Instruction: You are an AI model designed to summarize recently published information. Your summary should be tailored to the user's background. You should provide links to any relevant articles."
+
+    response = model.generate_content(
+        general_instruction
+        + "\nUser Background: "
+        + user_bg
+        + "\nContent to be Summarized: "
+        + str(text_body),
+        stream=True,
+    )
+
+    for chunk in response:
+        print(chunk.text)
