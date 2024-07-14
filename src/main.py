5c85040313496fb7474518adba4dc155260def38
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2024-07-14 18:25:03 +0200

    Change to astro-ph and print out links/titles at end

--- src/main.py
+++ src/main.py
@@ -13,15 +13,21 @@ def main():
 
     # Change these values as desired
     topics: list[str] = [
-        "cs.AI",
-        "econ.GN",
+        "astro-ph.EP",
+        "astro-ph.HE",
+        "astro-ph.CO",
+        "astro-ph.GA",
+        "astro-ph.IM",
+        "astro-ph.SR",
     ]  # see https://arxiv.org/category_taxonomy for a complete list of topics
 
-    user_bg = "I'm a machine learning engineer looking to stay up-to-date with the latest research in AI in order to stay competitive in the field. My work is primarily focused on the overlap between artifical intelligence and robotics, but I'm interested in hearing about research across the field. I also am interested in economic research that may be relevant to my life as an investor."
+    user_bg = "I'm an astronomer who studies supernovae and kilonovae. My observatory is particularly interested in 1A SNe, any research related to kilonovae, and general instrumental methods that could be applied to our facility."
     days_back = 4  # Number of days back to search
 
     print("Searching sources for relevant information...")
     info = aggregate(topics, days_back)
+
+    print(f"Found {len(info)} papers.")
 
     print("Distilling information...")
 
@@ -30,7 +36,9 @@ def main():
         text_body=info,
     )  # Print out is implemented in distill.py
 
-    ## TODO: Print out the titles and links to the info that was distilled
+    ## Print out the titles and links to the info that was distilled
+    for i in info:
+        print(i["title"] + ": " + i["url"])
 
 
 if __name__ == "__main__":
