50c8fe867f462656d892d8b14b272dece23ec7c6
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2024-07-16 14:16:21 +0200

    Write output to markdown file

--- src/distill.py
+++ src/distill.py
@@ -1,6 +1,6 @@
 import google.generativeai as genai
 from credentials import get_gemini_key
-import textwrap
+import datetime
 
 
 def distill(
@@ -11,6 +11,7 @@ def distill(
     Accepts the user's background (str) and the list of text bodies(list[str]) and returns a single piece of markdown.
     Markdown is produced by a gemini model (specified by the model parameter)
     """
+    file = open(f"output/distilled_info-{datetime.date.today().strftime("%Y%m%d%H%M%S")}.md", "w")
 
     gemini_key: str = get_gemini_key()
     genai.configure(api_key=gemini_key)
@@ -18,14 +19,20 @@ def distill(
 
     general_instruction = "General Instruction: You are an AI model designed to summarize recently published information. Your summary should be tailored to the user's background."
 
+    print(f"Tokens in gathered content: {model.count_tokens(str(text_body))}")
+
+
     response = model.generate_content(
         general_instruction
         + "\nUser Background: "
         + user_bg
         + "\nContent to be Summarized: "
-        + str(text_body),
-        stream=True,
+        + str(text_body)
     )
 
-    for chunk in response:
-        print(chunk.text)
+    file.write(response.text)
+
+    ## Print out the titles and links to the info that was distilled
+    file.write("\n\n## Source Information: \n")
+    for i in text_body:
+        file.write((i["title"] + ": " + i["url"] + "\n"))
