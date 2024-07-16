41a824ea4e2ff19ab3701b8e1dc5ecab0b62234e
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2024-07-16 18:45:21 +0200

    Placeholder

---
+++ src/perplexity.py
@@ -0,0 +1,41 @@
+## Will implement function to search newsapi.ai for relevant articles 
+from credentials import get_perplexity_key
+import requests
+
+
+def search_perplexity(user_bg: str) -> str:
+    # url = "https://api.perplexity.ai/chat/completions"
+
+    # payload = {
+    #     "model": "llama-3-70b-instruct",
+    #     "messages": [
+    #         {
+    #             "role": "system",
+    #             "content": "Provide the user with a good picture of current events/news relevant to their background. Cite your sources."
+    #         },
+    #         {
+    #             "role": "user",
+    #             "content": f"{user_bg}"
+    #         }
+    #     ],
+    #     "max_tokens": 2000,  # Adjust this value as needed
+    #     "temperature": 0.7,  # Add temperature for response variability
+    #     "return_citations": True
+    # }
+    # headers = {
+    #     "accept": "application/json",
+    #     "content-type": "application/json",
+    #     "authorization": f"Bearer {get_perplexity_key()}"
+    # }
+
+    # try:
+    #     response = requests.post(url, json=payload, headers=headers)
+    #     response.raise_for_status()  # This will raise an exception for HTTP errors
+    #     return response.json()
+    # except requests.exceptions.RequestException as e:
+    #     print(f"Error: {e}")
+    #     print(f"Response content: {response.text}")
+    #     return None
+    return "THIS IS A MOCK PERPLEXITY RESPONSE."
+
+print(search_perplexity("I am a student studing computer science and artifical intelligence. "))
