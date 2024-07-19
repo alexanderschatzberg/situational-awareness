0ef5a5039dc61cfbd829c0e012c98ae0a140d64c
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2024-07-19 14:58:39 +0200

    rm perplexity section

--- src/write_output.py
+++ src/write_output.py
@@ -1,6 +1,6 @@
 import datetime 
 
-def write_output(perplexity_res: str, gemini_summary: str, text_body) -> None:
+def write_output(gemini_summary: str, text_body) -> None:
     """
     A function that writes the output of the pipeline to a markdown file.
     Accepts the perplexity response, the gemini summary, and the text body.
@@ -9,7 +9,7 @@ def write_output(perplexity_res: str, gemini_summary: str, text_body) -> None:
 
     file.write(gemini_summary)
 
-    file.write("## Perplexity Response: \n" + perplexity_res)
+    # file.write("## Perplexity Response: \n" + perplexity_res)
 
     file.write("\n\n## Source Information: \n")
     for i in text_body:
